72c73a18706e1c8ec55bcf1eed976d6df5c681aa
Panaiot Uzunov <panaiot.uzunov@gmail.com>
2017-02-17 21:51:51 +0200

    Uploaded PrimeNumbersSmallerThanN (Windows Forms Application)

---
+++ PrimeNumbersSmallerThanN/PrimeNumbersSmallerThanN/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PrimeNumbersSmallerThanN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.listPrimes = new System.Windows.Forms.ListBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Location = new System.Drawing.Point(63, 68);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrint.TabIndex = 0;
+            this.buttonPrint.Text = "Print Primes";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(63, 42);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(276, 20);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // listPrimes
+            // 
+            this.listPrimes.FormattingEnabled = true;
+            this.listPrimes.Location = new System.Drawing.Point(63, 97);
+            this.listPrimes.Name = "listPrimes";
+            this.listPrimes.Size = new System.Drawing.Size(276, 186);
+            this.listPrimes.TabIndex = 2;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(264, 68);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(63, 13);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ReadOnly = true;
+            this.textBoxStatus.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStatus.TabIndex = 5;
+            this.textBoxStatus.Text = "Enter end number:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 326);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.listPrimes);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.buttonPrint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.ListBox listPrimes;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxStatus;
+    }
+}
+
---
+++ PrimeNumbersSmallerThanN/PrimeNumbersSmallerThanN/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeNumbersSmallerThanN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            listPrimes.DataSource = null; // при всяко ново натискане изчистваме списъка
+
+            int endNum = 0; // дефинираме променливата извън try-catch блока за да можем да я използваме извън него
+
+            try
+            {
+                endNum = int.Parse(this.textBoxInput.Text); // парсваме текста от textBoxInput
+                textBoxStatus.Text = "Number is OK.";
+            }
+            catch (Exception)
+            {
+                textBoxStatus.Text = "Invalid Number!"; // ако не успеем да парснем уведомяваме
+            }
+
+
+            List<int> primeNums = new List<int>();
+
+            for (int curentNum = 2; curentNum < endNum; curentNum++) // въртим до крайното число 
+            {
+                bool isPrime = true; // дефинираме булева променлиза за проверка за просто число 
+
+                for (int devider = 2; devider < curentNum; devider++) // въртим делителя
+                {
+                    if (curentNum % devider == 0)
+                    {
+                        isPrime = false;
+                        break; // ако числото се дели без остатък не е просто и няма смисъл да продължаваме цикъла
+                    }
+                }
+
+                if (isPrime)
+                    primeNums.Add(curentNum); // ако числото е просто добавяме в списък 
+            }
+
+            listPrimes.DataSource = primeNums; // принтираме числата в списъка
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            listPrimes.DataSource = null; // изчистваме списъка
+            textBoxInput.Text = null; // изчистваме полето за въвеждане
+        }
+    }
+}
