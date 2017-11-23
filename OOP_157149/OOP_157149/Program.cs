b7d5e1f0b0fb283b55cad1d94da3e737a5e1726b
Panaiot Uzunov <panaiot.uzunov@gmail.com>
2017-11-23 16:20:43 +0200

    Uploaded OOP_157149

---
+++ OOP_157149/OOP_157149/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_157149
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X objectOne = new X(); // създаваме обект от клас Х
+            objectOne.text = ReadXtext();
+            objectOne.number = ReadXnumber();
+        }
+
+        private static decimal ReadXnumber() // неясно условие относно свойствата на числото
+        {
+            decimal number = 0;
+
+            while (number == 0)
+            {
+                try
+                {
+                    Console.Write("Insert number parameter: ");
+                    number = decimal.Parse(Console.ReadLine());
+                }
+
+                catch
+                {
+                    Console.WriteLine("Incorrect number. Try again.");
+                }
+            }
+
+            return number;
+        }
+
+        private static string ReadXtext() // Прочитаме текст за Х, не позволяваме въвеждане на празен string
+        {
+            string text = "";
+            bool textIsEmpty = true;
+
+            while (textIsEmpty)
+            {
+                Console.Write("Insert text parameter: ");
+                text = Console.ReadLine();
+                textIsEmpty = text == "";
+
+                if (textIsEmpty)
+                    Console.WriteLine("Empty string is not accepted!");
+            }
+
+            return text;
+        }
+    }
+}
---
+++ OOP_157149/OOP_157149/X.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_157149
+{
+    class X
+    {
+        public string text { get; set; }
+        public decimal number { get; set; } // В условието не е описано дали числото да е цяло или дробно. Ще използвам decimal
+        // todo: статично поле за отчитане на броя на обектите от този клас.
+    }
+}
